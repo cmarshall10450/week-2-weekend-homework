88299fadf57c06ce2a13059af73aa44342b1fac1
Chris Marshall <cmarshall10450@gmail.com>
2017-10-06 15:39:56 +0100

    BarTab - test_bar_tab_has_amount

--- BarTab.rb
+++ BarTab.rb
@@ -1,12 +1,11 @@
 class BarTab
 
-	attr_reader :id, :room
+	attr_reader :id, :room, :amount
 
 	def initialize(id, room)
 		@id = id
 		@room = room
 		@amount = 0
 	end
-
 
 end
--- specs/BarTab.specs.rb
+++ specs/BarTab.specs.rb
@@ -4,15 +4,19 @@ require_relative('../BarTab')
 class TestBarTab < MiniTest::Test
     
   def setup
-    @barTab = BarTab.new(1, 'Room 101')
+    @bar_tab = BarTab.new(1, 'Room 101')
 	end
 
 	def test_bar_tab_has_id
-		assert_equal(1, @barTab.id)
+		assert_equal(1, @bar_tab.id)
 	end
 	
 	def test_bar_tab_has_room
-		assert_equal('Room 101', @barTab.room)
+		assert_equal('Room 101', @bar_tab.room)
+	end
+
+	def test_bar_tab_has_amount
+		assert_equal(0, @bar_tab.amount)
 	end
 
 end
