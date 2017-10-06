247192e60b704d8aa6f5f31cd0675d767d352c03
Chris Marshall <cmarshall10450@gmail.com>
2017-10-06 10:50:56 +0100

    Song - test_song_has_name passing

---
+++ specs/Song.specs.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require_relative('../Song')
+
+class TestSong < MiniTest::Test
+
+  def setup
+    @song = Song.new('99 Red Balloons', "Nina")
+	end
+
+	def test_song_has_name
+		expected = '99 Red Balloons'
+		result = @song.title
+		assert_equal(expected, result)
+	end
+
+end
