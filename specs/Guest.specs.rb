6a23466ba0255b828938e17185282fd3c2fd5ccc
Chris Marshall <cmarshall10450@gmail.com>
2017-10-06 15:06:09 +0100

    Guest - test_guest_has_favourite_song passing

--- specs/Guest.specs.rb
+++ specs/Guest.specs.rb
@@ -1,14 +1,20 @@
 require('minitest/autorun')
 require_relative('../Guest')
+require_relative('../Song')
 
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest = Guest.new('Chris', 100)
+		@song = Song.new('Burning Ring of Fire', 'Johnny Cash')
+    @guest = Guest.new('Chris', 100, @song)
 	end
 
 	def test_guest_has_name
 		assert_equal('Chris', @guest.name)
+	end
+
+	def test_guest_has_favourite_song
+		assert_equal(@song, @guest.favourite_song)
 	end
 
 	def test_guest_can_pay
